796adef9d6ed8998ecf4940d6d40325b512845da
desc0059 <ldescotes1@gmail.com>
2024-10-17 10:17:14 +0200

    sources

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Carousel from '@/components/carousel'
 import Footer from '@/components/footer'
+import Sources from '@/components/sources'
 
 export default function page() {
   return (
     <section className=' image h-screen w-full'>
       <Carousel />
+      <Sources />
       <Footer />
     </section>
   )

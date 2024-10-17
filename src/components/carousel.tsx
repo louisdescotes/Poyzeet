1b3d323e897dcb1b487b9342f6db5838f2ae7ecd
desc0059 <ldescotes1@gmail.com>
2024-10-17 08:50:07 +0200

    Text change & animation duration

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,75 +1,86 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react";
+import { act, useEffect, useState } from "react";
 
 interface PropsLoader {
- title : string,
- active ?: number,
- time?: number,
+  title: string;
+  active: number;
+  onChange: (index: number) => void;
 }
 
 interface PropsCarouselText {
-  information : number,
- }
+  information: number;
+}
 
- const carouselInformation = [
+const carouselInformation = [
   {
     titleLoader: "Concentration",
-    titleCarousel: "Concentration. ",
+    titleCarousel: "Boostez votre concentration. ",
     descCarousel:
-      "Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur? Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur? Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur?",
+      "Restez concentré, peu importe l'environnement. Le bruit blanc masque les distractions sonores et crée une atmosphère propice au travail ou à l'étude. Idéal pour maximiser votre productivité dans les espaces bruyants.",
   },
   {
     titleLoader: "Méditation",
-    titleCarousel: "Méditation",
+    titleCarousel: "Plongez dans la méditation",
     descCarousel:
-      "Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur? Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur? Lorem ipsum dolor sit amet.",
+      "Plongez dans un état de calme profond. Grâce à son son constant et apaisant, le bruit blanc favorise la relaxation et vous aide à vous recentrer. Parfait pour vos séances de méditation ou de relaxation.",
   },
   {
     titleLoader: "Endormissement",
-    titleCarousel: "Endormissement",
+    titleCarousel: "Trouvez le sommeil facilement",
     descCarousel:
-      "Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur? Lorem ipsum dolor sit amet. Et saepe rerum eum quasi beatae qui quos consequatur?",
+      "Dites adieu aux nuits agitées. Le bruit blanc crée un fond sonore stable qui masque les bruits gênants, facilitant ainsi l'endormissement et un sommeil plus profond.",
   },
 ];
 
+function Loader({ title, active, onChange }: PropsLoader) {
+  const isActive = carouselInformation[active].titleLoader === title;
 
-function Loader({ title} : PropsLoader) {
-  return <div className="cursor-pointer ">
-    <div className="relative w-[285px] h-1 bg-200">
-      {/* <div className="absolute top-0 w-4/12 h-full bg-400" /> */}
+  return (
+    <div className="cursor-pointer" >
+      <div className="relative w-[285px] h-1 bg-200">
+        <div
+          className={`absolute top-0 h-full w-full transition-all duration-[9800ms] origin-left ${
+            isActive ? "bg-400 scale-x-100" : "bg-transparent w-0"
+          }`}
+        />
+      </div>
+      <p className="text1 mt-1">{title}</p>
     </div>
-    <p className="text1 mt-1">{title}</p>
-  </div>;
+  );
 }
 
-function CarouselText({ information} : PropsCarouselText) {
+function CarouselText({ information }: PropsCarouselText) {
   const { titleCarousel, descCarousel } = carouselInformation[information];
 
   return (
-   <aside className="flex flex-col gap-1.5 w-full min-h-48">
-    <h5 className="title2 max-w-[600px]">{titleCarousel } </h5>
-    <p className="text1 max-w-[600px]">{descCarousel}</p>
-  </aside>
+    <aside className="flex flex-col gap-1.5 w-full min-h-48">
+      <h5 className="title2 max-w-[600px]">{titleCarousel} </h5>
+      <p className="text1 max-w-[600px]">{descCarousel}</p>
+    </aside>
   );
 }
 
 export default function Carousel() {
-  const [active, setActive] = useState(0)
-  
+  const [active, setActive] = useState(0);
+
   useEffect(() => {
     const increment = () => {
-      setActive(prevActive => prevActive >= 2 ? prevActive - 2 : prevActive + 1)
-    }
-    const interval = setInterval(increment, 3000);
+      setActive((prevActive) =>
+        prevActive >= 2 ? prevActive - 2 : prevActive + 1
+      );
+    };
+    const interval = setInterval(increment, 10000);
     return () => clearInterval(interval);
-  }, [])
-  
+  }, []);
+
   return (
     <section className="text-950 flex flex-col justify-center items-center gap-12 w-8/12 mx-auto h-full ">
       <nav className="flex items-center justify-between w-full gap-10">
         {carouselInformation.map((items, index) => {
-          return <Loader key={index} title={items.titleLoader}/>
+          return (
+            <Loader key={index} title={items.titleLoader} active={active} />
+          );
         })}
       </nav>
       <CarouselText information={active} />

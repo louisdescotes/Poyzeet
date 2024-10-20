cc70e9ffe64a5132152aec929328b5b62cf40b13
desc0059 <ldescotes1@gmail.com>
2024-10-20 22:04:16 +0200

    Menu done

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,76 +1,108 @@
 "use client";
+import * as motion from "framer-motion/client";
+import * as THREE from 'three'
 
+import React from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
-  Box,
+  MeshTransmissionMaterial,
   OrthographicCamera,
-  Scroll,
   ScrollControls,
   Text3D,
   useScroll,
 } from "@react-three/drei";
-import { useRef } from "react";
+import { useControls } from "leva";
 
-const text = [
-  "Avantages",
-  "Explication",
+const textArray = [
   "Index",
+  "Explication",
+  "Avantages",
   "Shop",
   "Contact",
+  "Autres",
+  "Index",
+  "Explication",
   "Avantages",
+  "Shop",
+  "Contact",
+  "Autres",
 ];
 
 function ScrollText() {
-  const radius = 6;
-  const angleIncrement = (Math.PI * 2) / text.length;
   const scroll = useScroll();
+  const radius = 5; 
 
-  const text3D = useRef()
+  const angleIncrement = (2 * Math.PI) / textArray.length;
+
+  const groupRefs = Array.from({ length: textArray.length }, () => React.createRef());
+
   useFrame(() => {
-    console.log(scroll.offset * 10); 
-    text3D.current.position.x *= scroll.offset
+    const offset = scroll.offset * 2 * Math.PI; 
+
+    groupRefs.forEach((ref, index) => {
+      const angle = index * angleIncrement + offset;
+      const x = radius * Math.cos(angle) + 5;
+      const y = radius * Math.sin(angle) - .5;
+      
+      ref.current.position.set(x, y, 0);
+    });
   });
 
   return (
     <>
-      <Scroll>
-        {text.map((text, index) => {
-          const angle = index * angleIncrement;
-          const x = radius * Math.cos(angle);
-          const y = -14.5 + index * 3;
-
-          useFrame(() => {
-
-          })
-          return (
-            <Text3D
-            ref={text3D}
-              key={index}
-              position={[x, y, -40]}
-              font="/Satoshi Medium_Regular.json"
-              height={0}
-            >
-              {text}
-              <meshMatcapMaterial color="#292929" />
-            </Text3D>
-          );
-        })}
-      </Scroll>
+      {textArray.map((text, index) => (
+        <group key={index} ref={groupRefs[index]}>
+          <Text3D
+            font="/Satoshi Medium_Regular.json"
+            height={0}
+          >
+            {text}
+            <meshMatcapMaterial color="#292929" />
+          </Text3D>
+        </group>
+      ))}
     </>
   );
 }
 
 export default function Landing() {
+  const config = ({
+    samples: 10,
+    resolution: 256,
+    transmission: 1,
+    roughness: 0.2,
+    thickness: 10,
+    ior: 1.38,
+    chromaticAberration: 0,
+    anisotropy: 1,
+    distortion: 0.4,
+    distortionScale: 0.64,
+    temporalDistortion: 0.26,
+    clearcoat: 1,
+    attenuationDistance: 10,
+    attenuationColor: '#ffffff',
+    color: '#ffffff',
+    bg: '#ffffff'
+  })
+
   return (
     <section className="landing w-full h-screen">
-      <Canvas>
-        <OrthographicCamera position={[0, 0, -40]} />
+      <Canvas className="bg-[#E1E1E1]">
+        <OrthographicCamera position={[0, 0, 0]} />
         <ScrollControls pages={2.95} damping={0.2} infinite>
           <ScrollText />
         </ScrollControls>
 
-        <Box args={[20, 2]} position={[4, 3.5, 0]} />
-        <Box args={[20, 2]} position={[4, -3.5, 0]} />
+        <mesh  position={[4, 3.5, 0]}>
+          <planeGeometry args={[20,2]}/>
+          <meshPhysicalMaterial {...config} /> : <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />}
+          </mesh>
+
+
+        <mesh  position={[4, -3.5, 0]}>
+          <planeGeometry args={[20,2]}/>
+           <meshPhysicalMaterial {...config} /> : <MeshTransmissionMaterial background={new THREE.Color(config.bg)} {...config} />}
+          </mesh>
       </Canvas>
     </section>
   );

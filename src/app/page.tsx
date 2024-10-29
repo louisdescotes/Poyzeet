44108719675f4c2fe491e92043eae7ea13b9b584
desc0059 <ldescotes1@gmail.com>
2024-10-29 19:50:26 +0100

    Relecture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Landing from "@/components/landing";
 import Footer from "@/components/nav/Footer";
 import Header from "@/components/nav/Header";
 import AdvantagesList from "@/components/sections/advantages/advantagesList";
-import Test from "@/components/test";
-
 export default function page() {
   return (
     <section className=" image h-screen w-full">

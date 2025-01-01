2ae1c43dd9a89420d582179ed7c6304daa114a42
desc0059 <ldescotes1@gmail.com>
2025-01-01 16:15:56 +0100

    Lecteur video

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,20 +1,8 @@
-import Store from "../../utils/store";
-import Dots from "../dots";
-
 export default function Nav() {
-  const { openMenu, menu } = Store();
 
   return (
-    <nav className="fixed top-5 left-0 px-10 py-2.5 w-full flex items-center justify-between text-50 z-50">
+    <nav className="fixed top-5 left-0 px-10 py-2.5 w-full flex items-center justify-center z-50 mix-blend-difference">
         <img src="/svg/logo.svg" alt="logo de l'entreprise" />
-        <div onClick={openMenu} className="flex items-center gap-2 cursor-pointer z-50">
-          <p>Menu</p>
-          <Dots />
-        </div>
-
-        {menu && 
-        <div className="fixed top-0 left-0 w-screen h-screen bg-black">
-        </div>}
     </nav>
   )
 }
--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,13 +1,57 @@
-export default function Video() {
-    return (
-      <section>
-        {/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
-        <video controls>
-            <source src="/video/video1.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-      </section>
+import { useRef, useState } from "react";
+import "./style.css";
 
-    );
-  }
-  
+const Video = () => {
+  const video = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const handlePausePlay = () => {
+    if (isPlaying) {
+      video.current.pause();
+    } else {
+      video.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleMetadata = () => {
+    setDuration(video.current.duration); 
+  };
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(video.current.currentTime); 
+  };
+
+  const calculateProgress = () => {
+    return (currentTime / duration) * 100; 
+  };
+
+  return (
+    <section className="relative">
+      {/* biome-ignore lint/a11y/useMediaCaption: <explanation> */}
+      <video
+        className="cursor-pointer"
+        onLoadedMetadata={handleMetadata}
+        onTimeUpdate={handleTimeUpdate}
+        ref={video}
+        src="/video/video1.mp4"
+        onClick={handlePausePlay}
+        aria-label="Video player"
+      />
+
+      <div className="relative w-full h-1 bg-gray-600 overflow-hidden cursor-pointer">
+        <div
+          className="slider absolute left-0 h-full bg-200"
+          style={{
+            width: `${calculateProgress()}%`,
+            transition: "width 0.1s linear", 
+          }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Video;
